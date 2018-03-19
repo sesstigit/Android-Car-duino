d6eedee8eca85c9264c3eb08a9419886ad1e7f04
SES <sesstigit@gmail.com>
2018-03-19 16:09:43 +1030

    added GPL license header

--- app/src/main/jni/Autodrive/src/Car.h
+++ app/src/main/jni/Autodrive/src/Car.h
@@ -1,3 +1,38 @@
+/**
+*    This file is part of Autodrive.
+*
+*    Autodrive is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Autodrive is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with Autodrive.  If not, see <http://www.gnu.org/licenses/>.
+**/
+ 
+#pragma once
+#ifndef ANDROIDCARDUINO_AUTODRIVE_AUTODRIVE_H_
+#define ANDROIDCARDUINO_AUTODRIVE_AUTODRIVE_H_
+
+#include "Car.h"
+#include "ParkingManeuver.h"
+#include "imageprocessor/ImageConfig.h"
+//using namespace std;
+
+
+namespace Autodrive {
+    //ImageConfig conf;  //image processing configuration settings
+    Car car;  //car methods can now be called from JNI
+    //ParkingManeuver park(car, ParkingManeuverMode::kNoManeuver);
+}
+
+#endif //ANDROIDCARDUINO_AUTODRIVE_AUTODRIVE_H_
+ 
 #pragma once
 #ifndef ANDROIDCARDUINO_AUTODRIVE_CAR_H_
 #define ANDROIDCARDUINO_AUTODRIVE_CAR_H_
